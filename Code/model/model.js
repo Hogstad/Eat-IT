b60fc34fbd236c17df2096903dc30c9fdd3ea574
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-01-19 13:25:22 +0100

    v0.11

--- Code/model/model.js
+++ Code/model/model.js
@@ -1,1 +1,78 @@
 //model
+const model = {
+
+category: {
+    varamretter: {
+        taco: {
+            ingredients: [
+                {ingredients: 'Kjøttdeig', isSelected: true },
+                {ingredients: 'tacokrydder', isSelected: true },
+                {ingredients: 'tortilla', isSelected: true },
+                {ingredients: 'isbergsalat', isSelected: true },
+                {ingredients: 'slangeagurk', isSelected: true },
+                {ingredients: 'tomater', isSelected: true },
+                {ingredients: 'tomater', isSelected: true },
+                {ingredients: 'mais', isSelected: true },
+                {ingredients: 'gulost', isSelected: true },
+                {ingredients: 'rømme', isSelected: true },
+            ],
+        },
+        
+    },
+    salat: {
+        tacoSalat : {
+            ingredients: [
+                {ingredients: 'karbonadedeig', isSelected: true },
+                {ingredients: 'hvitløk', isSelected: true },
+                {ingredients: 'olje', isSelected: true },
+                {ingredients: 'tacokrydder', isSelected: true },
+                {ingredients: 'vann', isSelected: true },
+                {ingredients: 'røde bønner', isSelected: true },
+                {ingredients: 'rødløk', isSelected: true },
+                {ingredients: 'sitron', isSelected: true },
+                {ingredients: 'hjertesalat', isSelected: true },
+                {ingredients: 'tortillachips', isSelected: true },
+                {ingredients: 'vårløk', isSelected: true },
+                {ingredients: 'jalapeñog', isSelected: true },
+                {ingredients: 'frisk koriander', isSelected: true },
+             ],
+        },
+        
+    },
+
+    dessert: {
+        dessert : {
+            ingredients: [
+                {ingredients: 'dessert', isSelected: true },
+             ],
+        },
+        
+    },
+
+    drikke: {
+        drikke : {
+            ingredients: [
+                {ingredients: 'drikke', isSelected: true },
+             ],
+        },
+    },
+
+    leggtilny: {
+        legg til ny : {
+            ingredients: [
+                {ingredients: 'legg til ny', isSelected: true },
+             ],
+        },
+    },
+},
+
+
+orderWaiter
+-navn på retten
+-kommentar på retten
+-checkbox
+
+orderCook
+-navn på retten
+-kommentar på retten
+-checkbox
