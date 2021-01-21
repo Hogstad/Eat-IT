776664242a39d9da1f05a4f08cb879227154e5fb
thuve92 <72792411+thuve92@users.noreply.github.com>
2021-01-21 13:06:05 +0100

    index/mainView changes

---
+++ Code/view/mainView.js
@@ -0,0 +1,33 @@
+const divApp = document.getElementById("app");
+
+
+
+mainView();
+function mainView() {
+    let html = "";
+    html += `
+           <button>Servitør</button>
+           <button>Legg til ny bruker</button>
+           <button>Ordre</button>
+           <ul>`;
+           
+
+    //        <li> <button>Varmretter</button </li>
+    //        <li> <button>Salat</button </li>
+    //        <li> <button>Dessert</button </li>
+    //        <li> <button>Drikke</button </li>
+    //        <li> <button>Legg til ny</button </li>
+           
+    //        </ul>
+
+    //        <h3>Taco:${model.category.mainCourse.taco.ingredients.length}</h3>
+                      
+
+    //        <button>Rediger</button>
+    //        <button>Legg til ny</button>
+           
+    
+    
+    
+    // divApp.innerHTML = html;
+}
