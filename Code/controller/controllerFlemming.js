23e454ec2587dd74caaface36504935b491ee850
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-02-04 10:31:03 +0100

    Update controllerFlemming.js

--- Code/controller/controllerFlemming.js
+++ Code/controller/controllerFlemming.js
@@ -26,13 +26,12 @@
 // function checkbox
 // -checkbox til ordre siden. 
 
-function addNewContentInBox() {
+function saveNewRetter() {
     let x = {
-        name: addCategory,
+        name: addRetter,
         ingredients: [],
     }
     model.category[model.selectedCategory].retter.push(x)
-subView();
 }
 
 function selectedCategory(num) {
