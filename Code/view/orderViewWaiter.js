6e0428cb6d72613d89318185f8090e8764a9ca94
thuve92 <72792411+thuve92@users.noreply.github.com>
2021-02-10 12:55:52 +0100

    update

--- Code/view/orderViewWaiter.js
+++ Code/view/orderViewWaiter.js
@@ -23,6 +23,20 @@ function orderViewWaiter() {
                 <button onclick="orderViewWaiter()">Ordre</button>
             </div> 
             `;
+            html += `
+                <div>
+                    <table>
+                        <th>Bord 1</th>
+                        <th>Henrik</th>
+                        <tr>
+                        <td>Taco</td>
+                        <td>Ingr</td>
+                        <td><input type="checkbox"></td>
+                        </tr>
+                    </table>
+                </div>
+            
+            `;
          
         divApp.innerHTML = html;
 };
