2ff85c9977fc8bf1bc85fe1f265f2a565541cc08
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-02-10 12:34:33 +0100

    Update orderViewCook.js

--- Code/view/orderViewCook.js
+++ Code/view/orderViewCook.js
@@ -1,33 +1,30 @@
 // skjermbilde 7, 8!!!!!!!!!!!!!!!!!!!
-
 function orderViewCook() {
     let html = "";
     html += `
             <div id="mainViewButtons">
-                <button>Servitør</button>
+            <select onchange="selectedWaitor(this.value)"> `;
+            for(let i = 0; i <model.waitor.names.length; i++) {
+                if (i == model.selectedWaitor) {
+                    html += `
+                    <option selected value="${i}">${model.waitor.names[i].name}</option>
+                    `;
+                }
+                else {
+                    html += `
+                    <option value="${i}">${model.waitor.names[i].name}</option>
+                    `;
+                }
+            }
+            html += `
+            </select>
                 <button>Legg til ny bruker</button>
-                <button>Ordre</button>
+                <button onclick="orderViewWaiter()">Ordre</button>
             </div> 
-                `;
-                html += `
-                <div id="oneDiv">
-                `;
-            for(let i = 0; i < model.category.length; i++) {
-                html += `
-                    <button id="mainViewCategory">${model.category[i].type}</button>    
-               `;
-            };
-            html += `
-                <button id="addCategory">Legg til</button>
-            </div>
-            `;
-            html += `
-            <div id="mainViewShowDishes">${model.showDishes}</div>
             `;
          
         divApp.innerHTML = html;
 };
-
 
 //Sette inn table for alle greiene i model.category[].retter[].ingredients 
 // Table med type på toppen "navn"
