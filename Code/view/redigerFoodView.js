ff731773b539b72765e57faf38e0c998a74cbacf
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-02-15 13:13:19 +0100

    Update redigerFoodView.js

--- Code/view/redigerFoodView.js
+++ Code/view/redigerFoodView.js
@@ -61,8 +61,7 @@ function redigerFoodView(index) {
                 </table>
                     <br>
                     <br>
-                    <input style="width: 235px;" type="text" placeholder=""></input>
-                    <button style="position: relative; top: 25px; left: -72px;">Send inn</button>
+                    <button style="position: relative; top: 25px; left: -72px;">Rediger</button>
             </div>  
             `;
             
